e06a2d7f9fbf4ddad7796c3c004a6b76246a7628
k-m-rahman <k_m_rahman23@hotmail.com>
2022-10-25 14:40:45 +0600

    added some fake courses data

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const courses = require("./data/courses.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("passage to learning server is running");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
